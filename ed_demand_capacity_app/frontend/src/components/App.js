a71835ad5a49aa005c7d8c7f3f9def6554766d4e
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-05-25 20:56:05 +0100

    Rewrite app component as function based instead of class based

--- ed_demand_capacity_app/frontend/src/components/App.js
+++ ed_demand_capacity_app/frontend/src/components/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+// export default class App extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    render() {
-        return <h1> Testing React Code</h1>;
-    }
-}
+//     render() {
+//         return <h1> Testing React Code</h1>;
+//     }
+// }
 
     
+// const appDiv = document.getElementById("app");
+// render(<App />, appDiv);
+
+export default function App(props) {
+    return (
+        <h1> Testing React Code</h1>
+    );
+}
+
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App/>, appDiv);
