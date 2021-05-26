b3ef00637d7412b8ae4532ab5ad7932c04133214
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-05-26 13:38:49 +0100

    Add display of static sample data to historic data tab

--- ed_demand_capacity_app/frontend/src/pages/HistoricDemandData.js
+++ ed_demand_capacity_app/frontend/src/pages/HistoricDemandData.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
+import { render } from 'react-dom';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+// export default function HistoricDemandData() {
+//     return (
+//         <Typography variant="h3"> 
+//         Historic Demand Data (Placeholder)
+//         </Typography>
+//     )
+// }
 
 export default function HistoricDemandData() {
+    const rowData = [
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxter", price: 72000}
+    ];
+ 
     return (
-        <Typography variant="h3"> 
-        Historic Demand Data (Placeholder)
-        </Typography>
-    )
-}
+        <div className="ag-theme-material" style={{height: 400, width: 600}}>
+            <AgGridReact
+                rowData={rowData}>
+                <AgGridColumn field="make"></AgGridColumn>
+                <AgGridColumn field="model"></AgGridColumn>
+                <AgGridColumn field="price"></AgGridColumn>
+            </AgGridReact>
+        </div>
+    );
+ };
+ 
