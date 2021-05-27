16031eb1e23b8e26cbf733211e57b07deb02022d
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-05-27 18:18:41 +0100

    Make buttons bring up file picker

--- ed_demand_capacity_app/frontend/src/pages/HistoricDemandData.js
+++ ed_demand_capacity_app/frontend/src/pages/HistoricDemandData.js
@@ -21,47 +21,57 @@ import {
     //const classes = useStyles();
 
     return (
-    <Grid container spacing={1}>
-    
-    <Grid container item xs={6}>
-        <Card paddingBottom={4}>
-            <CardContent>
-            <Typography variant='h4'>
-                Is your data in record format?
-            </Typography>
-            <Typography variant='h6'>
-                <br/>
-                Record format data means you have one row per patient. 
-                <br/><br/>
-                Your data needs to contain columns for arrival date, arrival time, and stream.
-                <br/><br/>
-            </Typography>
-            <Button color="primary" variant="contained">
-                Upload record-format data
-            </Button>
-            </CardContent>   
-        </Card>
-    </Grid>
+        <div>
+            <Grid container spacing={1}>
+            
+            <Grid container item xs={6}>
+                <Card paddingBottom={4}>
+                    <CardContent>
+                    <Typography variant='h4'>
+                        Is your data in record format?
+                    </Typography>
+                    <Typography variant='h6'>
+                        <br/>
+                        Record format data means you have one row per patient. 
+                        <br/><br/>
+                        Your data needs to contain columns for arrival date, arrival time, and stream.
+                        <br/><br/>
+                    </Typography>
+                    <Button color="primary" variant="contained" component="label">
+                        Upload record-format data
+                        <input
+                            type="file"
+                            hidden
+                        />
+                    </Button>
+                    </CardContent>   
+                </Card>
+            </Grid>
 
-    <Grid container item xs={6}>
-        <Card paddingBottom={4}>
-            <CardContent>
-            <Typography variant='h4'>
-                Is your data being imported from the Excel model?
-            </Typography>
-            <Typography variant='h6'>
-                <br/>
-                If you hvae previously filled in the Excel model, you can upload the Excel file
-                to extract the historic data in it. 
-                <br/><br/>
-            </Typography>
-            <Button color="primary" variant="contained">
-                Upload Excel Model
-            </Button>
-            </CardContent>   
-        </Card>
-    </Grid>
+            <Grid container item xs={6}>
+                <Card paddingBottom={4}>
+                    <CardContent>
+                    <Typography variant='h4'>
+                        Is your data being imported from the Excel model?
+                    </Typography>
+                    <Typography variant='h6'>
+                        <br/>
+                        If you have previously filled in the Excel model, you can upload the Excel file
+                        to extract the historic data in it. 
+                        <br/><br/>
+                    </Typography>
+                    <Button color="primary" variant="contained" component="label">
+                        Upload Excel Model
+                        <input
+                            type="file"
+                            hidden
+                        />
+                    </Button>
+                    </CardContent>   
+                </Card>
+            </Grid>
 
-    </Grid>
-    );
-    }
+        </Grid>
+    </div>
+            );
+            }
