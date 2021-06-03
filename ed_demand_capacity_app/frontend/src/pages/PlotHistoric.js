ae3c55fad0f78def9cdfa82405abb751b1d48798
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-03 12:23:15 +0100

    Add simple visualisation in plotly of monthly data

--- ed_demand_capacity_app/frontend/src/pages/PlotHistoric.js
+++ ed_demand_capacity_app/frontend/src/pages/PlotHistoric.js
@@ -1,6 +1,11 @@
+/* global Plotly:true */
+
 import React, { Component, useState, useEffect } from "react";
-import Plot from 'react-plotly.js';
+// import Plot from 'react-plotly.js';
 import axios from 'axios';
+
+import createPlotlyComponent from 'react-plotly.js/factory'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 
@@ -31,58 +36,116 @@ import axios from 'axios';
 
 //     }
 
-function getPlotData() {
-  fetch('/api/most-recently-uploaded-historic-data-plotly-ms')
-  // axios({
-  //               "method": "GET",
-  //               "url": "http://localhost:8000/api/most-recently-uploaded-historic-data-plotly-ms",
-  //             })
-      .then((response) => response.json())
-      .then((mydata) => {
-          this.setState({
-              data: mydata.data,
-          })
-          console.log(mydata.data);
-      });
-}
+// function getPlotData() {
+//   fetch('/api/most-recently-uploaded-historic-data-plotly-ms')
+//   // axios({
+//   //               "method": "GET",
+//   //               "url": "http://localhost:8000/api/most-recently-uploaded-historic-data-plotly-ms",
+//   //             })
+//       .then((response) => response.json())
+//       .then((mydata) => {
+//           this.setState({
+//               data: mydata.data,
+//           })
+//           console.log(mydata.data);
+//       });
+// }
+
+const Plot = createPlotlyComponent(Plotly);
 
 export default class PlotHistoric extends Component {
-    constructor(props) {
-      super(props);
-      
-      this.state = {
-        data: [{
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: {color: 'red'},
-              },
-              {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},], 
-        layout: {}, 
-        frames: [], 
-        config: {}};
+  constructor(props) {
+    super(props);
 
-      // this.getPlotData = this.getPlotData.bind(this);
-    }
+    // this.handleJsonChange = this.handleJsonChange.bind(this);    
 
-    componentDidMount () {
-      getPlotData()
-    } 
-  
-    render() {
-      console.log(this.state.data)
+    const defaultPlotJSON = {
+        data: [],
+        layout: {
+            plotBackground: '#f3f6fa',
+            margin: {t:0, r: 0, l: 20, b: 30},
+          }
+      };
+
+    this.state = {
+        json: defaultPlotJSON,
+        loaded: false
+    };
+  }
+
+  componentDidMount () { 
+    fetch('/api/most-recently-uploaded-historic-data-plotly-ms')
+        .then((response) => {
+            return response.json();
+            
+        })
+        
+        .then((data) => {
+          // It's necessary to use the next line as for some reason
+          // the server is returning a json-like object rather than
+          // a valid json
+          var fixedJson = $.parseJSON(data);
+            this.setState({
+              json: fixedJson,
+              loaded: true
+            });
+          // console.log(data)
+  });
+}
+
+  // handleJsonChange = newJSON => {
+  //     this.setState({json: newJSON});
+  // }
+
+  // handleNewPlot = option => {
+  //     let url = '';
+  //     if ('value' in option) {
+  //         url = option.value;
+  //     }
+  //     else if ('target' in option) {
+  //         url = option.target.value;
+  //         if (url.includes('http')) {
+  //             if (!url.includes('.json')) {
+  //                 url = url + '.json'
+  //             }
+  //         }
+  //     }
+
+  //     if(url) {
+  //         fetch(url)
+  //         .then((response) => response.json())
+  //         .then((newJSON) => {
+  //             if ('layout' in newJSON) {    
+  //                 if ('height' in newJSON.layout) {
+  //                     newJSON.layout.height = null;
+  //                 }
+  //                 if ('width' in newJSON.layout) {
+  //                     newJSON.layout.width = null;
+  //                 }
+  //             }
+  //             this.setState({
+  //                 json: newJSON,
+  //                 plotUrl: url
+  //             });
+  //         });
+  //     }
+  // }
+
+  render() {
+    if (!this.state.loaded) {
       return (
-        <h6>Yo</h6>
-        // <Plot
-        //   data={this.state.data}
-        // //   layout={this.state.layout}
-        //   layout={ {width: 920, height: 640, title: 'A Fancy Plot'} }
-        //   frames={this.state.frames}
-        //   config={this.state.config}
-        //   onInitialized={(figure) => this.setState(figure)}
-        //   onUpdate={(figure) => this.setState(figure)}
-        // />
+        <CircularProgress />
       );
+    } else {
+      return (           <div>
+      <Plot
+          data={this.state.json.data}
+          layout={this.state.json.layout}
+          config={{displayModeBar: false}}
+      />
+      {console.log(this.state.json)}
+  </div>    );
+      
     }
   }
+}
