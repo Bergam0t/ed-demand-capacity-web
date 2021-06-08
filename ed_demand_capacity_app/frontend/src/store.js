8bab913274ece0f516b60561bc6d54bc4830dccf
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-08 16:19:19 +0100

    Add logout toast notification

--- ed_demand_capacity_app/frontend/src/store.js
+++ ed_demand_capacity_app/frontend/src/store.js
@@ -1,17 +1,40 @@
 import { action } from 'easy-peasy';
+import { toast } from 'react-toastify';
+
+
+// Notifications
+const notifyLogout = () => toast.warn('Logged Out Successfully', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
 
 export default {
-    // States
+    
+    // ---- States ---- //
     loggedIn: localStorage.getItem('token') ? true : false,
 
-    // Actions
+    // ---- Actions ---- //
+
+    // Logging in 
     loggedInTrue: action((state) => {
         state.loggedIn = true;
     }),
 
+
+    // Logging Out
+
+
+
     logoutAction: action((state) => {
         localStorage.removeItem('token');
         state.loggedIn = false;
+        notifyLogout();
     })
 
 };
