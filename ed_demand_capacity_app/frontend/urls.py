29b3989b7abb9ecd2dfccddd3491b9377964e544
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-08 14:44:23 +0100

    Allow persistent navigation
    Previously going to a specific url would return a 404, whereas navigating there via the app would work fine. Now either will work.

--- ed_demand_capacity_app/frontend/urls.py
+++ ed_demand_capacity_app/frontend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import index
 
 # Needed to allow redirection from other apps 
@@ -7,4 +7,11 @@ app_name = 'frontend'
 urlpatterns = [
     # name needs to be set if redirecting here from a different app
     path('', index, name=''),
+]
+
+urlpatterns += [
+    # match the root
+    re_path(r'^$', index),
+    # match all other pages
+    re_path(r'^(?:.*)/?$', index),
 ]
