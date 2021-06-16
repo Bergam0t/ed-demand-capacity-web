b45ae536b0ec471eea8d127d1c1b554aa8cd139d
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-16 19:43:54 +0100

    Add component to display dataset in ag-grid

---
+++ ed_demand_capacity_app/frontend/src/components/LoadedExistingDataset.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+
+export default class DisplayExistingData extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.fetchData = this.fetchData.bind(this);
+
+        this.state = {
+            json: null,
+            loaded: false
+        };
+
+    }
+
+    fetchData() {
+        const loggedIn = localStorage.getItem('token') ? true : false
+
+        if (loggedIn) {
+        return fetch(this.props.api_url, {
+            headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`
+            }
+        }) } else {
+            return fetch(this.props.api_url)
+        }
+    };
+
+
+    componentDidMount () { 
+        this.fetchData()
+       .then((result) => {return result.json();})
+       .then((data) => {
+            this.setState({
+                  json: data,
+                  loaded: true
+                })
+            }
+        );
+    };
+
+
+    render() {
+        if (!this.state.loaded) {
+            return (
+              <CircularProgress />
+            );
+          } else {
+            return (
+                <div className="ag-theme-alpine" style={{height: 800, width: 1000}}>
+                    <AgGridReact
+                        rowData={this.state.json}
+                    >
+                        <AgGridColumn field='date' />
+                        <AgGridColumn field='stream' />
+                        <AgGridColumn field='nhs_number' />
+                        <AgGridColumn field='arrival_time' />
+                    </AgGridReact>
+                </div>
+            );
+          }
+        }
+    };
