c156a7c7b7c687c702271b59154ee5733491bedf
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-22 21:46:00 +0100

    Add title to demand forecast plot page

--- ed_demand_capacity_app/frontend/src/pages/RequiredVsAvailableCapacity.js
+++ ed_demand_capacity_app/frontend/src/pages/RequiredVsAvailableCapacity.js
@@ -1,12 +1,17 @@
 import React, { Component} from "react";
 import PlotlyPlot from "../components/PlotlyPlot" 
 // import DisplayExistingData from "../components/LoadedExistingDataset"
+import Typography from '@material-ui/core/Typography';
+
 
 export default class PlotForecast extends Component {
     render() {
         return (
+        <div>
+        <Typography variant="h4"> Predicted Demand: Admissions Per Hour for Next 7 Days</Typography>
         <PlotlyPlot api_url="/api/most-recently-uploaded-data-forecast" />
-        // <DisplayExistingData api_url='/api/most-recently-uploaded-ag-grid-json' />
+        {/* // <DisplayExistingData api_url='/api/most-recently-uploaded-ag-grid-json' /> */}
+        </div>
         )
     }
 };
