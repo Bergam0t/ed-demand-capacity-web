49ad537def011e022b4e1ddedf87ead6116c669a
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-22 21:26:17 +0100

    Add title to historic plot

--- ed_demand_capacity_app/frontend/src/pages/PlotHistoric.js
+++ ed_demand_capacity_app/frontend/src/pages/PlotHistoric.js
@@ -1,10 +1,14 @@
 import React, { Component} from "react";
 import PlotlyPlot from "../components/PlotlyPlot" 
+import Typography from '@material-ui/core/Typography';
 
 export default class PlotForecast extends Component {
     render() {
         return (
+        <div>
+        <Typography variant = "h5"> Monthly Admissions by Stream </Typography>
         <PlotlyPlot api_url="/api/most-recently-uploaded-historic-data-plotly-ms" />
+        </div>
         )
     }
 };
