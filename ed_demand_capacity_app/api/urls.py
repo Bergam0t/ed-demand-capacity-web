07d88a70f3e7c7ecd41fa654703b2e8f4b9ad2ba
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-06-22 22:39:30 +0100

    Tidy URLs file

--- ed_demand_capacity_app/api/urls.py
+++ ed_demand_capacity_app/api/urls.py
@@ -3,40 +3,19 @@ from .views import *
 from .views_forecasting import *
 
 
+urlpatterns = []
 
-urlpatterns = [
-    path('organisation', 
-         OrganisationView.as_view()),
+
+# --- Historic Data --- #
+
+# Historic data upload
+urlpatterns += [
 
     path('historic-data', 
          HistoricDataView.as_view(), 
          name='historic_data_list'),
 
-    path('most-recently-uploaded-historic-data', 
-         DisplayMostRecentlyUploadedRawData.as_view(), 
-         name='most_recent_hist'),
-
-    path('most-recently-uploaded-historic-data-own', 
-         DisplayMostRecentlyUploadedOwnRawData.as_view(), 
-         name='most_recent_hist'),
-    
-    path('most-recently-uploaded-historic-data-pandas', 
-         MostRecentAsPandas.as_view(), 
-         name='most_recent_hist_pandas'),
-
-    path('most-recently-uploaded-historic-data-plotly-ms', 
-         PlotlyTimeSeriesMostRecent.as_view(), 
-         name='most_recent_hist_plotly_ms'),
-
-     path('most-recently-uploaded-data-forecast', 
-         ProphetForecastOneWeekMajors.as_view(), 
-         name='most_recent_forecast'),
-
-     path('most-recently-uploaded-ag-grid-json', 
-         MostRecentAsAgGridJson.as_view(), 
-         name='most_recent_ag_grid_json'),
-
-     path('session-has-historic-data', 
+    path('session-has-historic-data', 
          SessionHasHistoricData.as_view(), 
          name='session_has_historic_data'),
 
@@ -51,15 +30,63 @@ urlpatterns = [
     path('filter-by-cols-and-overwrite-data', 
         FilterByColsAndOverwriteData.as_view(),  
         name='filter_by_cols_and_overwrite_data'),
-     
+    ]
 
-
-     # path('get-user', 
-     #      UserDetailsFromToken.as_view(), 
-     #      name="user_details_from_token"),
-
-]
-
+# Historic data viewing (tabular)
 urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
+    path('most-recently-uploaded-historic-data', 
+         DisplayMostRecentlyUploadedRawData.as_view(), 
+         name='most_recent_hist'),
+
+    path('most-recently-uploaded-historic-data-own', 
+         DisplayMostRecentlyUploadedOwnRawData.as_view(), 
+         name='most_recent_hist'),
+    
+    path('most-recently-uploaded-historic-data-pandas', 
+         MostRecentAsPandas.as_view(), 
+         name='most_recent_hist_pandas'),
 ]
+
+# Paths for historic data plots
+urlpatterns += [
+    path('most-recently-uploaded-historic-data-plotly-ms', 
+         PlotlyTimeSeriesMostRecent.as_view(), 
+         name='most_recent_hist_plotly_ms'),
+
+     path('most-recently-uploaded-data-forecast', 
+         ProphetForecastOneWeekMajors.as_view(), 
+         name='most_recent_forecast'),
+
+     path('most-recently-uploaded-ag-grid-json', 
+         MostRecentAsAgGridJson.as_view(), 
+         name='most_recent_ag_grid_json'),
+
+]
+
+
+# --- Forecasting --- #
+
+# Forecasting plots
+     
+urlpatterns += [
+     path('most-recently-uploaded-data-forecast', 
+         ProphetForecastOneWeekMajors.as_view(), 
+         name='most_recent_forecast'),
+]
+
+
+# --- Authentication --- #
+
+# *TODO* Check whether I actually used this in the end!
+urlpatterns += [
+    path('api-auth/', 
+         include('rest_framework.urls')
+         ),
+]
+
+# --- Misc --- #
+
+urlpatterns+= [
+    path('organisation', 
+         OrganisationView.as_view()),
+         ]
