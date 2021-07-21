f778022e925baacab206f9923e56ea467fe5612f
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-07-21 09:53:00 +0100

    Create Rotas.js

---
+++ ed_demand_capacity_app/frontend/src/pages/Rotas.js
@@ -0,0 +1,68 @@
+import React, { Component, useState, useReducer, useEffect } from "react";
+import {
+    Box,
+    Container,
+    Grid,
+    Typography,
+    Button,
+    CardContent,
+    Modal,
+    ButtonGroup,
+    Divider,
+  } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import { useStoreActions } from 'easy-peasy';
+import {useStoreState} from 'easy-peasy';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns'; 
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import IconButton from '@material-ui/core/IconButton';
+import CancelIcon from '@material-ui/icons/Cancel';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import moment from 'moment'
+import { withStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { toast } from 'react-toastify';
+import PlotlyPlot from "../components/PlotlyPlot" 
+import { v4 as uuidv4 } from 'uuid';
+
+const useStyles = makeStyles((theme) => ({
+
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+        paddingLeft: theme.spacing(4),
+        paddingRight: theme.spacing(4),
+      },
+
+          // Add formatting for the resulting table
+    tableHead: {
+        background: '#cdcdcd',
+     },
+
+     tableHeadCell: {
+        fontWeight: 800,
+     }
+
+    })
+);
+
