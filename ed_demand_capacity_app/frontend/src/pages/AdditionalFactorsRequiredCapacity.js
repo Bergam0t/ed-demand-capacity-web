e45bb279a23e0e784a22191771fe457709420bf9
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2021-07-27 15:28:31 +0100

    WIP: Begin creating modal for entering factors and table for displaying factors

--- ed_demand_capacity_app/frontend/src/pages/AdditionalFactorsRequiredCapacity.js
+++ ed_demand_capacity_app/frontend/src/pages/AdditionalFactorsRequiredCapacity.js
@@ -10,6 +10,11 @@ import {
     ButtonGroup,
     Divider,
     Card,
+    Tooltip,
+    InputAdornment,
+    Select,
+    MenuItem,
+    Input
   } from '@material-ui/core';
 import { useStoreState, useStoreActions } from 'easy-peasy';
 import { makeStyles } from '@material-ui/core/styles';
@@ -27,6 +32,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import HelpIcon from '@material-ui/icons/Help';
 
 import DeleteIcon from '@material-ui/icons/Delete';
 import { toast } from 'react-toastify';
@@ -75,14 +81,15 @@ const useStyles = makeStyles((theme) => ({
 );
 
 
-export default function EDSettings() {
+export default function AdditionalFactorsRequiredCapacity() {
 
     const classes = useStyles();
 
     const loggedIn = useStoreState(state => state.loggedIn)
 
     // Initialise state for determining whether the page has finished loading
-    const [loaded, setloaded] = React.useState(false)
+    const [factorsLoaded, setFactorsLoaded] = React.useState(false)
+    const [streamsLoaded, setStreamsLoaded] = React.useState(false)
 
     // Function to get relevant request headers depending on whether the user is logged
     // in or not
@@ -97,6 +104,198 @@ export default function EDSettings() {
                 'content-type': 'application/json'
               }
         }};
+
+    // ----------------- //
+    // Streams 
+    // ----------------- //
+
+    const [streams, setStreams] = React.useState(null)
+
+    const fetchStreams = () => {
+        /**
+         * Fetch a list of streams from the API
+         * Sorts streams by priority
+         * Updates following states: streams, streamsOriginal, streamsLoaded
+         */
+        return fetch('api/get-historic-data-streams-from-db')
+            // Make sure to not wrap this first then statement in {}
+            // otherwise it returns a promise instead of the json
+            // and then you can't access the email attribute 
+            .then(response => 
+                response.json()
+            )
+            .then((json) => {
+                // Sort the returned states by priority
+                // From https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+                const sorted_json = json.sort((a, b) => {
+                    return a.stream_priority - b.stream_priority
+                }).slice()
+                setStreams(sorted_json);
+                
+            })
+            .then(() => setStreamsLoaded(true))
+      };
+
+
+    // ------------------------- //
+    // Modal for entering factors
+    // ------------------------- //
+
+
+      // Handle factor description
+    const [factorDescription, setFactorDescription] = useState('');
+
+    const [percentageChange, setPercentageChange] = useState(0);
+
+    const [increaseOrDecrease, setIncreaseOrDecrease] = useState('increase');
+
+    const [addFactorOpen, setAddFactorOpen] = React.useState(false)
+
+
+
+
+    function factorEntryModal() {
+        return (
+          <div>
+            <Dialog
+            open={addFactorOpen}
+            onClose={() => setAddFactorOpen(false)}
+            classes={{
+                root: classes.dialog,
+                paper: classes.paper
+            }}
+            disableBackdropClick
+            >
+              <Box>  
+              <Grid container style={{paddingLeft: 20, paddingRight: 20, paddingBottom:20}}>
+                <Grid item xs={6} align="left">
+                  <DialogTitle>
+                      Create a new factor
+                  </DialogTitle>
+                </Grid>
+                <Grid item xs={6} align="right">
+                  <IconButton onClick={() => setAddFactorOpen(false)} >
+                      <CancelIcon />
+                  </IconButton>
+                </Grid>
+            
+                <Grid item xs={12}>
+                  <DialogContentText>
+                      Here you can specify whether the factor leads to an increase or decrease in required capacity, as well as
+                      what time period the factor acts over.
+                  </DialogContentText>
+                </Grid>
+    
+                <Grid item xs={10}>
+                        <TextField
+                            required
+                            fullWidth
+                            margin="dense"
+                            id="resource-name"
+                            label="Factor Description"
+                            type="text"
+                            onChange={(e) => setFactorDescription(e.target.value)}
+                        />
+                        <br /><br /><br />
+                </Grid>
+                <Grid item xs={2}>
+                  <Tooltip 
+                    title="Record the description of the factor (e.g. hot weather)"
+                    classes={{tooltip: classes.tooltip}}>
+                    <IconButton aria-label="delete">
+                      <HelpIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Grid>
+                
+                <Grid container spacing={4}>
+                    <Grid item xs={4}>
+                            <Input
+                                required
+                                fullWidth
+                                margin="dense"
+                                id="percentage-change"
+                                label="Percentage Change"
+                                type="number"
+                                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+                                value = {percentageChange}
+                                onChange={(e) => e.target.value < 0 ? setPercentageChange(0) : setPercentageChange(e.target.value)}
+                            />
+                            <br /><br /><br />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Select
+                            labelId="increase-or-decrease-label"
+                            id="increase-or-decrease"
+                            value={increaseOrDecrease}
+                            label="Increase or Decrease"
+                            onChange={(e) => setIncreaseOrDecrease(e.target.value)}
+    
+                        >
+                            <MenuItem key="increase" value="increase">Increase</MenuItem>
+                            <MenuItem key="decrease" value="decrease">Decrease</MenuItem>
+                        </Select>
+                    </Grid>
+                </Grid>
+
+
+                
+
+                </Grid>
+                </Box>
+                </Dialog> 
+                </div>
+                )  
+        }
+
+
+    // -------------------- //
+    // Display of factors
+    // -------------------- //
+
+    function displayFactors() {
+        if (factorsLoaded && streamsLoaded) {
+    
+          return (
+            <div>
+                <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                
+                    <TableHead className={classes.tableHead}>
+                        <TableRow className={classes.tableHeadCell}>
+                            <TableCell className={classes.tableHeadCell}>Factor</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Percentage Change</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Increase or Decrease?</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Streams Affected</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Factor Type</TableCell>
+                            
+                            <TableCell className={classes.tableHeadCell}>Start Date</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Start Time</TableCell>
+                            
+                            <TableCell className={classes.tableHeadCell}>End Date</TableCell>
+                            <TableCell className={classes.tableHeadCell}>End Time</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </div>)
+            } else {
+            return (
+                <div>
+                    <CircularProgress />
+                </div>
+            )
+            }
+        }
+
+    // Determine what will run on page load
+    // Get streams from server
+    useEffect(() => {
+        fetchStreams()     
+    }, []);
 
 
     return (
@@ -114,6 +313,19 @@ export default function EDSettings() {
             <br />
             - changes to the required decision making time (e.g. due to expected delays in test results being returned)
             
+
+          <br /> <br />  
+          <Button 
+            variant="contained"
+            color="primary"
+            onClick={() => setAddFactorOpen(true)}>
+              Add Factor
+          </Button>
+
+        {factorEntryModal()}
+        <br /> <br />
+        {displayFactors()}
+
         </Typography>
         </div>
         )
